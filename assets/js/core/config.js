8efde5f226d4c9289fb09342a4efa00fd4374bd2
lucasvenancio0110 <lucassantanals0110@gmail.com>
2026-06-28 13:09:06 -0300

    Point frontend to Neodent CNC API

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -1,1 +1,1 @@
-window.NC_CONFIG={appName:"Neodent CNC",version:"neodent-cnc-cockpit-v1",apiBase:"https://passagem-turno-api.lucassantanals0110.workers.dev"};
+window.NC_CONFIG={appName:"Neodent CNC",version:"neodent-cnc-cockpit-v1",apiBase:"https://neodent-cnc-api.lucassantanals0110.workers.dev"};
